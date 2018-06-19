ac6d1789ebeaecde9243d71f417f4957c379cdec
wangzhaoli <329119628@qq.com>
2018-06-19 16:56:45 +0800

    ok

---
+++ src/page/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+      index
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style>
+.header {
+    display: flex;
+    height: 0.42rem;
+    background:pink;
+}
+</style>
